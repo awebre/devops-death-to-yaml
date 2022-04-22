cfc256b463ad0903cfe3aa10765349a7d542ec20
Austin Webre <austin.webre@gmail.com>
2022-04-22 18:49:31 -0500

    working on package/deployment

--- Deploy/Deploy.ps1
+++ Deploy/Deploy.ps1
@@ -1,19 +1,27 @@
+param (
+    [string]$packagePath
+)
+
 Write-Output "Logging in to Azure via service principal..."
 az login `
     --service-principal `
     --username $Env:AZ_CLIENT_ID `
     --password $Env:AZ_CLIENT_SECRET `
     --tenant $Env:AZ_TENANT_ID 
-Write-Output "Done logging in..."
+Write-Output "Successfully logged in!"
 
 Write-Output "Setting azure subscription..."
 az account set --subscription $Env:AZ_SUBSCRIPTION_ID
-Write-Output "Done setting azure subscription..."
+Write-Output "Done setting azure subscription!"
 
 Write-Output "Creating App Service Plan: ${$Env:APP_SERVICE_PLAN}"
 az appservice plan create --name $Env:APP_SERVICE_PLAN --resource-group $Env:RESOURCE_GROUP
-Write-Output "Done creating App Service Plan..."
+Write-Output "Done creating App Service Plan!"
 
 Write-Output "Creating Web App: ${$Env:WEB_APP_NAME}"
 az webapp create --name $Env:WEB_APP_NAME --plan $Env:APP_SERVICE_PLAN --resource-group $Env:RESOURCE_GROUP
-Write-Output "Done creating Web App..."
+Write-Output "Done creating Web Ap!"
+
+Write-Output "Publishign package to Web App: ${$Env:WEB_APP_NAME}"
+az webapp deployment source config-zip --name $Env:WEB_APP_NAME --resource-group $Env:RESOURCE_GROUP --src $packagePath
+Write-Output "Done publishing package to Web App!"
--- DevOps/Build.ps1
+++ DevOps/Build.ps1
@@ -1,9 +1,17 @@
-param ($deploy)
+param (
+    [switch]$deploy,
+    [string]$packageDir
+)
 
-dotnet build
+dotnet build --configuration Release
 dotnet test
 
-# If we are set for deployment and the previous have succeeded
-if($deploy -eq $true -and $?) {
-    Write-Host "We should deploy now!"
+# If we are set for deployment and the previous commands  have succeeded
+if ($deploy -eq $true -and $?) {
+    Write-Output "Publishing to $packageDir.zip...."
+    dotnet publish --configuration Release --output $packageDir
+    Compress-Archive $packageDir "$packageDir.zip"
+    Write-Output "Successfully published to $packageDir.zip"
+
+    ./Deploy/Deploy.ps1
 }
